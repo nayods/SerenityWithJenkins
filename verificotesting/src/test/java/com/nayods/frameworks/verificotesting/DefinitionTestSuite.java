4a7790ae7a2e486021dcb5b393441505094be15c
Nayods <nayods@boxmail.veridone.net>
2018-01-26 13:29:32 +0000

    # WARNING: head commit changed in the meantime
    commiting
    
    Change-Id: I763db4a600111294f7efd40acb5c5cb083281b2c

--- verificotesting/src/test/java/com/nayods/frameworks/verificotesting/DefinitionTestSuite.java
+++ verificotesting/src/test/java/com/nayods/frameworks/verificotesting/DefinitionTestSuite.java
@@ -5,5 +5,5 @@ import net.serenitybdd.cucumber.CucumberWithSerenity;
 import org.junit.runner.RunWith;
 
 @RunWith(CucumberWithSerenity.class)
-@CucumberOptions(features="src/test/resources/features")
+@CucumberOptions(features= {"src/test/resources/features"},plugin= {"pretty","json:target/cucumber.json"})
 public class DefinitionTestSuite {}
---
+++ verificotesting/src/test/resources/features/Ecommerce_Search/BuyProductFeature.feature
@@ -0,0 +1,7 @@
+Feature: customer want to purchase products on 
+ the ecommerce webpage
+
+Scenario: customers product purchase
+GIVEN customer is on the ecpommerce webpage
+WHEN they add product to the basket
+THEN the customer should see the ammount to pay.
