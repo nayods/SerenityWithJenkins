36df116cb52f800416786b70e3a2a39b7caec353
Nayods <nayods@boxmail.veridone.net>
2018-01-26 16:52:59 +0000

    commit with @tag

--- verificotesting/src/test/java/com/nayods/frameworks/verificotesting/DefinitionTestSuite.java
+++ verificotesting/src/test/java/com/nayods/frameworks/verificotesting/DefinitionTestSuite.java
@@ -5,5 +5,7 @@ import net.serenitybdd.cucumber.CucumberWithSerenity;
 import org.junit.runner.RunWith;
 
 @RunWith(CucumberWithSerenity.class)
-@CucumberOptions(features= {"src/test/resources/features"},plugin= {"pretty","json:testData/cucumber.json"})
+@CucumberOptions(features= {"src/test/resources/features"},
+tags= {"@tag"},
+plugin= {"pretty","json:testData/cucumber.json"})
 public class DefinitionTestSuite {}
