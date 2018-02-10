589a3c7a32ef7e8d7573bf0087674385a578b492
Nayods <nayods@boxmail.veridone.net>
2018-02-10 19:58:39 +0000

    commits

--- verificotesting/src/test/java/com/nayods/frameworks/verificotesting/DefinitionTestSuite.java
+++ verificotesting/src/test/java/com/nayods/frameworks/verificotesting/DefinitionTestSuite.java
@@ -1,11 +1,12 @@
 package com.nayods.frameworks.verificotesting;
 
 import cucumber.api.CucumberOptions;
+import cucumber.api.SnippetType;
 import net.serenitybdd.cucumber.CucumberWithSerenity;
 import org.junit.runner.RunWith;
 
 @RunWith(CucumberWithSerenity.class)
-@CucumberOptions(features= {"src/test/resources/features"},
+@CucumberOptions(features= {"src/test/resources/features"}, snippets= SnippetType.CAMELCASE,
 tags= {"~@ignore","~@tag"},
 plugin= {"pretty","json:testData/cucumber.json"})
 public class DefinitionTestSuite {}
